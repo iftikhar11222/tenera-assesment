d572bf60c5b62d37e3dd49899c9d85396d90b039
ecom <engr.iftikhar100@hotmail.comk>
2021-06-27 22:58:13 +0500

    project setup

---
+++ src/main/java/com/tenera/assesment/TeneraAssesmentApplication.java
@@ -0,0 +1,13 @@
+package com.tenera.assesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeneraAssesmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeneraAssesmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tenera/assesment/TeneraAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenera.assesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeneraAssesmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
